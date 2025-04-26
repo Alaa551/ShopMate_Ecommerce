2d13276e3ad7855b9f1fb11d899b312b60d57739
mai <miaoesh@gmail.com>
2025-04-26 12:00:53 +0300

    working on BLL

---
+++ ShopMate/ShopMate.BLL/DTO/AdminDto/ContactMEssageDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopMate.BLL.DTO.AdminDto
+{
+    public class ContactMessageDto
+    {
+        public int Id { get; set; }                    
+        public string? UserFullName { get; set; }      
+        public string? Subject { get; set; }             
+        public string? Message { get; set; }           
+        public DateTime CreatedAt { get; set; }         
+        public string? Status { get; set; }              
+    }
+
+    public class ContactMessageReplyDto
+    {
+        public string? MessageId { get; set; }               
+        public string? Reply { get; set; }                
+    }
+}
---
+++ ShopMate/ShopMate.BLL/DTO/AdminDto/ProductReviewDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopMate.BLL.DTO.AdminDto
+{
+    public class ProductReviewDto
+    {
+        public int Id { get; set; }                
+        public string? ProductName { get; set; }    
+        public string? UserFullName { get; set; }      
+        public string? Comment { get; set; }             
+        public int Rating { get; set; }                 
+        public DateTime ReviewDate { get; set; }       
+    }
+}
